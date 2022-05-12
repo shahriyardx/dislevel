2ef9e9d55e587aeea92a20f1d9e0956282cd55e9
shahriyardx <contact@shahriyar.dev>
2022-05-13 03:21:56 +0600

    Alpha 1

--- dislevel/_version.py
+++ dislevel/_version.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 
-__version__ = "2.0.0a0"
+__version__ = "2.0.0a1"
 
 
 @dataclass
@@ -12,4 +12,4 @@ class VersionInfo:
     serial: int
 
 
-version_info = VersionInfo(2, 0, 0, "alpha", 0)
+version_info = VersionInfo(2, 0, 0, "alpha", 1)
