d6fc8752d2fb479ce35ecaf3364a387a40dab025
shahriyardx <contact@shahriyar.dev>
2022-05-13 15:13:34 +0600

    Release stable 2.0.0

--- dislevel/_version.py
+++ dislevel/_version.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 
-__version__ = "2.0.0a1"
+__version__ = "2.0.0"
 
 
 @dataclass
@@ -12,4 +12,4 @@ class VersionInfo:
     serial: int
 
 
-version_info = VersionInfo(2, 0, 0, "alpha", 1)
+version_info = VersionInfo(2, 0, 0, "stable", 0)
