4a02a81ac7a977e45d84262aaffa0e14d29c072d
Md Shahriyar Alam <mdshahriyaralam552@gmail.com>
2022-05-15 19:46:11 +0600

    Bumped version 2.0.1b0 -> 2.0.1

--- dislevel/_version.py
+++ dislevel/_version.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 
-__version__ = "2.0.1b0"
+__version__ = "2.0.1"
 
 
 @dataclass
@@ -12,4 +12,4 @@ class VersionInfo:
     serial: int
 
 
-version_info = VersionInfo(2, 0, 1, "beta", 0)
+version_info = VersionInfo(2, 0, 1, "stable", 0)
